e731dca00a9549fb17266de1de39b2f9e5d32657
Daniel Munoz <dsantiagomj@gmail.com>
2026-02-27 11:11:22 -0500

    chore(transactions): add barrel export for filter components

--- apps/web/src/features/transactions/ui/components/index.ts
+++ apps/web/src/features/transactions/ui/components/index.ts
@@ -1,5 +1,8 @@
+export { CategoryCascadePicker } from './CategoryCascadePicker';
+export { CategoryFilterField } from './CategoryFilterField';
 export { CategoryPickerContent } from './CategoryPickerContent';
 export { CategoryPickerField } from './CategoryPickerField';
 export { DatePickerContent } from './DatePickerContent';
 export { DatePickerField } from './DatePickerField';
 export { DateRangeContent } from './DateRangeContent';
+export { DateRangeFilter } from './DateRangeFilter';
